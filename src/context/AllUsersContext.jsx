eada57fed3cc6430a0357fb50f1e63f8b36d54d2
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-02-22 17:55:47 +0000

    contexts added

---
+++ src/context/AllUsersContext.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useMemo, useRef } from "react";
+import usersReducer from "../reducers/allUsersReducer";
+
+export const AllUsersStateContext = createContext();
+export const AllUsersDispatchContext = createContext();
+
+
+export default function AllUsersProvider({ childern }){
+   
+    const initialUserState = {
+        allUsers: [],
+        usersAreLoading: true
+    };
+
+    const [state, dispatchAllUsers] = useAsyncReducer(
+        usersReducer,
+        initialUserState
+    );
+
+    return (
+        <AllUsersStateContext.Provider value={state}>
+            <AllUsersDispatchContext value={dispatchAllUsers}>
+                { childern }
+            </AllUsersDispatchContext>
+        </AllUsersStateContext.Provider>
+    );
+}
+
