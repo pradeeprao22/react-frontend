0647a28d53c40abb4baec3dd000d717ff77d8c17
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-02-22 14:13:52 +0000

    reducers add

---
+++ src/reducers/allUsersReducer.jsx
@@ -0,0 +1,26 @@
+import { getAllUsers } from "../services/users";
+
+export const usersReducer = (state, action) => {
+    return new Promise(async (resolve) => {
+        const { type, payload } = action;
+
+        switch (type) {
+            case "INIT":
+                try {
+                  const allUsers = await getAllUsers();
+                  resolve({ ...state, allUsers, usersAreLoading: false });
+                } catch (error) {
+                  return state;
+                }
+                return state;
+            
+            case "USER_CREATED":
+                try {
+                    reslove({ ...state, allUsers: [...state.allUsers, payload] });
+                } catch (error) {
+                    return state;
+                }
+                return state;
+        }
+    });
+};
