af2de7bc315c88e4b316c4555115b83b7f5c0d8b
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-02-23 13:12:48 +0000

    post data fetch

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -4,33 +4,41 @@ import { getAllPosts } from "../../services/posts"
 
 export default function Post() {
     const [posts, setResponseData] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
-    const fetchData = (e) => {
-        e.preventDefault()
+    // const fetchData = (e) => {
+    //     e.preventDefault()
 
-        getAllPosts()
-        .then((resonse)=>{
-            setResponseData(resonse.data);
-            console.log(resonse)
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
+    //     getAllPosts()
+    //     .then((resonse)=>{
+    //         setResponseData(resonse.data);
+    //         console.log(resonse)
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     })
+    // }
 
-    // useEffect(() => {
-    //     const fetchPosts = async () => {
-    //       const postData = await getAllPosts();
-    //       setResponseData(postData);       
-    //     };
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const postData = await getAllPosts();
+          setResponseData(postData);
+          setLoaded(true);
+          console.log(postData)
+        };
 
-    //     fetchPosts();
-    // });
-
-    // const [ ] = useState()
+        fetchPosts();
+    });
 
     return (
-        <button onClick={(e) => fetchData(e)} type='button'>Click Me</button>
+        <div>
+          {posts.map((post) => (
+
+            <div>{post.id}</div>,
+            <div>{post.frontend}</div>
+
+          ))}
+          </div>
     );
 }
 
--- src/services/posts.js
+++ src/services/posts.js
@@ -4,7 +4,7 @@ export const getAllPosts = async () => {
    try {
         const resp = await api.get("/posts");
         // debugger;
-        console.log(resp.data)
+        // console.log(resp.data)
         return resp.data;
    } catch(error) {
         return error;
