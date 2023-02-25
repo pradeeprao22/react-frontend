6b4ca7b0fc323580f547b78958b086d6773774ba
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-02-25 10:31:45 +0000

    register changes

--- src/components/Login/Register.jsx
+++ src/components/Login/Register.jsx
@@ -1,9 +1,25 @@
-import react from "react";
+import { useStateValue } from "../../context/CurrentUsersContext";
 import { useState, useContext } from "react";
+import { registerUser } from "../../services/auth";
+import { useNavigate } from "react-router-dom";
+import { AllUsersDispatchContext, AllUsersStateContext } from "../../context/AllUsersContext"
 
 export default function Register() {
+    const[{ currentUser }, dispatch] = useStateValue()
+    const dispatchAllUsers =  useContext(AllUsersDispatchContext)
+    const history = useNavigate() 
 
-    const [formState, setFormState] = useState({
+
+    const handelRegister = async (registerData) => {
+        registerData.email = registerData?.email?.toLowerCase();
+        const userData = await registerUser(registerData);
+        dispatch({ type: "SET_USER", currentUser: userData })
+        dispatchAllUsers({ type: "USER_CREATED", payload: userData })
+
+        history.push("/")
+    };
+
+    const [formData, setFormData] = useState({
         email: "",
         password: "",
         confirmPassword: "",
@@ -15,45 +31,50 @@ export default function Register() {
 
     const handelChange = (e) => {
         const { name, value } = e.target;
-        setFormState((prevState) => ({
+        setFormData((prevState) => ({
             ...prevState,
             [name]: value,
         }));
     };
 
-    const handelSubmit = () => {
-
+    const handelSubmit = (e) => {
+        e.preventDefault()
+        handelRegister(formData)
     };
 
 
     return(
         <div className="container">
         <h1>Register</h1>
-        <form >
+        <form onSubmit={handelSubmit}>
             <label for="exampleInputEmail1" className="form-label">Email address</label>
             <input 
-             type="email" 
+             type="text" 
              className="form-control" 
              id="exampleInputEmail1" 
              aria-describedby="emailHelp"
+             name="email"
+             onChange={handelChange}
              
              />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
 
             <label for="exampleInputPassword1" className="form-label">Password</label>
             <input 
-             type="password" 
+             type="text" 
              className="form-control" 
              id="exampleInputPassword1"
+             name="password"
+             onChange={handelChange}
              
              />
 
             <label for="exampleInputPassword1" className="form-label">Confirm Password</label>
-            <input 
-            
-             type="confirmpassword" 
+            <input
+             type="text" 
              className="form-control" 
              id="exampleInputPassword1"
+             name="confirmpassword"
+             onChange={handelChange}
              
             />
 
