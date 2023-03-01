ca71ab89b6339290522ce771c5ec86d96a408c4c
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-01 09:20:05 +0000

    new case add to reducers

--- src/reducers/allUsersReducer.jsx
+++ src/reducers/allUsersReducer.jsx
@@ -5,23 +5,49 @@ export const usersReducer = (state, action) => {
         const { type, payload } = action;
 
         switch (type) {
-            case "INIT":
-                try {
-                  const allUsers = await getAllUsers();
-                  resolve({ ...state, allUsers, usersAreLoading: false });
-                } catch (error) {
-                  return state;
-                }
-                return state;
-            
-            case "USER_CREATED":
-                try {
-                    reslove({ ...state, allUsers: [...state.allUsers, payload] });
-                } catch (error) {
+                case "INIT":
+                    try {
+                    const allUsers = await getAllUsers();
+                    resolve({ ...state, allUsers, usersAreLoading: false });
+                    } catch (error) {
                     return state;
-                }
-                return state;
-                
+                    }
+                    return state;
+            
+                case "USER_CREATED":
+                    try {
+                        reslove({ ...state, allUsers: [...state.allUsers, payload] });
+                    } catch (error) {
+                        return state;
+                    }
+                    return state;
+            
+                case "USER_UPDATED":
+                    try {
+                      resolve({
+                        ...state,
+                        allUsers: state.allUsers.map((user) =>
+                          user.id === Number(payload.id) ? payload : user
+                        ),
+                      });
+                    } catch (error) {
+                      return state;
+                    }
+                    return state;
+            
+                case "USER_REMOVED":
+                    try {
+                      resolve({
+                        ...state,
+                        allUsers: state.allUsers.filter(
+                          (user) => user.id !== Number(payload.id)
+                        ),
+                      });
+                    } catch (error) {
+                      return state;
+                    }
+                    return state;
+
             default:
                 return state;    
         }
