8e88155eb8f3d335a670e781a44d3e00304712e0
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-04 12:48:53 +0000

    logo.svg removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useMemo, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import List from "./components/List"
 // import AddToList from "./components/AddToList"
