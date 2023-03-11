7e169e215264bbd7065e2179f2cf08d58dbfcb8b
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-11 10:46:50 +0000

    syntex error changes

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Nest as ProvidersNest } from './components/Healpers/Nest';
 import { appProviders } from "./utils/appProviders";
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Router>
-    <ProvidersNest element={appProviders}>
+    <ProvidersNest elements={appProviders}>
       <App />
     </ProvidersNest>
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
