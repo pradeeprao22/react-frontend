8fd57a1f4950f1b0b0ccba936ca410b6cbdbe106
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-11 11:11:03 +0000

    syntex error change

--- src/components/Healpers/Nest.jsx
+++ src/components/Healpers/Nest.jsx
@@ -1,7 +1,7 @@
 import { cloneElement } from "react";
 
-export const Nest = ({ element, children }) => 
-    element.reduceRight(
+export const Nest = ({ elements, children }) => 
+    elements.reduceRight(
         (out, element) => cloneElement(element, {}, out),
-        <>{ children }</>
+        <>{children}</>
     );
