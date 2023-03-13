cc10543f13e4747fda12a3c971508a158babde82
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-13 11:51:03 +0000

    private route add

---
+++ src/Router/PrivateRouter.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+    const token = localStorage.getItem("authToken");
+
+    return (
+      <Route
+        {...rest}
+        render={(props) => {
+          return token ? <Component {...props} /> : <Redirect to="/login" />;
+        }}
+      />
+    );
+}
