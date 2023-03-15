4a2bc61169dec898bc295bc2ee365a6419c5b814
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-15 10:46:40 +0000

    usercontainer add

--- src/containers/UsersContainer.jsx
+++ src/containers/UsersContainer.jsx
@@ -1,13 +1,23 @@
 import { useContext, useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { getOneUser } from "../services/users";
 import { 
     AllUsersDispatchContext,
     AllUsersStateContext,
-} from "../context/AllUsersContext"
+  } from "../context/AllUsersContext"
 
 export default function UserContainer() {
     const { allUsers, usersLoading } = useContext(AllUsersStateContext)
     const dispatchAllUsers = useContext(AllUsersDispatchContext)
+
+    useEffect(()=> {
+        const fetchUsers = async()=> {
+          dispatchAllUsers({ type: "INIT"});
+        };
+        fetchUsers();
+    }, [])
+    return(
+        <></>
+    );
 
 }
