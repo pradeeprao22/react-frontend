8f580f9fe263681c5701d343c5942d21d8cd5822
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-17 10:27:00 +0000

    currentUser add to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,10 @@
+import { useStateValue } from "../context/CurrentUserContext";
+
 export default function Dashboard() {
+    const currentUser = useStateValue();
+
     return(
+        console.log(currentUser),
         <div className="container">
             <h1>Dashboard</h1>
         </div>
