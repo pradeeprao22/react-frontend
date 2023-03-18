80f3ed9443d7fa0398eedb9dababd4fe0b2cfd9e
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-18 10:54:47 +0000

    bootstrap navbar add

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -25,6 +25,37 @@ import Post from "../components/Posts/Post"
 export default function AppRouter() {
   return (
       <div>
+
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand">Londevs</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link"><Link to="/">Home</Link><span class="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link"><Link to="/">Login</Link></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link"><Link to="/">Register</Link></a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          USERIFUSER
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a className="dropdown-item" ><Link to="/">Login</Link></a>
+          <a className="dropdown-item" >Register</a>
+          <a className="dropdown-item" >Dashboard</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
         <ul>
           <li>
             <Link to="/">Login</Link>
