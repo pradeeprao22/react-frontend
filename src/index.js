371dcf523f653a656f58f21c36ce1c177db9a8a2
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-19 10:16:08 +0000

    bootstrap add

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Nest as ProvidersNest } from './components/Healpers/Nest';
 import { appProviders } from "./utils/appProviders";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
