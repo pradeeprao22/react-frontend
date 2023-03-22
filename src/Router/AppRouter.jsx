231b18a93587b668e83ce0ad091286cc0653aeb0
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-22 11:02:27 +0000

    current user in router

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -50,13 +50,21 @@ export default function AppRouter() {
       </li>
       <li className="nav-item dropdown">
         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          USERIFUSER
+        {
+                (() => {
+                    if(currentUser) {
+                            return (
+                              <div>
+                                <p>{currentUser?.email}</p>
+                              </div>                          )
+                        } else {
+                            return (
+                              <a className="nav-link"><Link to="/">Login</Link></a>
+                              )
+                        }
+                })()  
+          }
         </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a className="dropdown-item" ><Link to="/">Login</Link></a>
-          <a className="dropdown-item" >Register</a>
-          <a className="dropdown-item" >Dashboard</a>
-        </div>
       </li>
     </ul>
   </div>
