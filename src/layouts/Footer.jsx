d29de5f3a407bd9f7363fd7f5b1c2530f04d790c
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-03-23 11:12:48 +0000

    layout add

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useStateValue } from "../context/CurrentUserContext";
+
+export default function Footer() {
+    // const [{currentUser}] = useStateValue();
+
+    return(
+        <div className="container">
+            <p>This is footer</p>
+        </div>
+    );
+}
